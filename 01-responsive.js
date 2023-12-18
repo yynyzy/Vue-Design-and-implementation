3af016ab261a53bb00d147ef9f11cb7f57264111
Hakurei Yan <hakurei.yan@maiscrm.com>
2023-12-18 11:00:39 +0800

    vue: observe ref obj study

--- 01-responsive.js
+++ 01-responsive.js
@@ -1,20 +1,38 @@
-const bucket = new Set(); // 桶，收集副作用函数
+// 桶结构，收集副作用函数, WeakMap<any, Map<any, Set<any>>>
+const bucket = new WeakMap();
 
 // 保存当前注册的副作用函数
 let activeEffect = '';
 
 const dataSource = {
-  text: '123'
+  text: 'observe obj before change'
 }
 
 const obj = new Proxy(dataSource, {
   get(target, key) {
-    bucket.add(activeEffect);
+    let target_in_bucket = bucket.get(key);
+    // 往 bucket 中注册目标对象
+    if(!target_in_bucket) {
+      target_in_bucket = new Map();
+      bucket.set(target, target_in_bucket)
+    }
+
+    let deps = target_in_bucket.get(key);
+    // 在 bucket 中目标对象的每一个key 添加对应的副作用函数，使用 Set 防止出现重复的副作用函数，
+    if(!deps) {
+      deps = new Set();
+      target_in_bucket.set(key, deps);
+    }
+    deps.add(activeEffect);
     return target[key];
   },
   set(target, key, newVal) {
     target[key] = newVal;
-    bucket.forEach((fn) => fn());
+    let target_in_bucket = bucket.get(target);
+    if(!target_in_bucket) return;
+    // 获取桶中对应 target 的对象并且拿到对应 key 的副作用 Set，如果存在，触发他们
+    let effects = target_in_bucket.get(key);
+    effects && effects.forEach((fn) => fn());
     return true;
   },
 })
@@ -26,5 +44,5 @@ const effect = (fn) => {
 effect(()=>{ document.body.innerHTML = obj.text });
 
 setTimeout(()=> {
-  obj.text = '999';
+  obj.text = 'observe obj after change';
 }, 2000);
